812c35c1417a4f3a9e678db18106fc0ed0fc930d
jefersonmmacedo <jefersonmacedowgf@gmail.com>
2023-10-26 11:11:52 -0300

    Links

--- src/services/websocket.js
+++ src/services/websocket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 //const socket = io('http://localhost:3333/');
-const socket = io('https://sua-chave-api.herokuapp.com/');
+const socket = io('https://suachave-api-private-e41301cb6190.herokuapp.com/');
 
 export {socket}
